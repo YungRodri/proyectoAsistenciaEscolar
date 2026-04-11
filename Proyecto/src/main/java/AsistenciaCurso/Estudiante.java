d3d9357c2cb4c39bd72272684b4d40e790304edf
Gonzalo Leal <gonzalo.leal.m@mail.pucv.cl>
2026-04-11 16:41:17 -0400

    Update Estudiante.java

--- Proyecto/src/main/java/AsistenciaCurso/Estudiante.java
+++ Proyecto/src/main/java/AsistenciaCurso/Estudiante.java
@@ -40,7 +40,107 @@ public class Estudiante extends Persona{
     }
 
     
-  
+  public void agregarAsistencia(Asistencia asistencia)
+    {
+        listaAsistencia.add(asistencia);
+    }
+
+    public Asistencia buscarAsistencia(String id)
+    {
+        int largo = listaAsistencia.size();
+        Asistencia asistencia_cmp;
+        for(int i = 0; i < largo; i++)
+        {
+            asistencia_cmp = listaAsistencia.get(i);
+            if(asistencia_cmp.getId().equals(id))
+            {
+                return asistencia_cmp;
+            }
+        }
+        return null;
+    }
+    public boolean eliminarAsistencia(String id)
+    {
+        for ( int i = 0; i < listaAsistencia.size(); i++)
+        {
+            if(listaAsistencia.get(i).getId().equals(id))
+            {
+                listaAsistencia.remove(i);
+                return true;
+            }
+        }
+        return false;
+    }
+    public boolean editarAsistencia(String id,  String observacion)
+    {
+        Asistencia asistencia_cmp = buscarAsistencia(id);
+        if(asistencia_cmp != null)
+        {
+            asistencia_cmp.setObservacion(observacion);
+            return true;
+        }
+        return false;
+    }
+    public void mostrarHistorial()
+    {
+        Asistencia asistencia_cmp;
+        for(int i = 0; i < listaAsistencia.size(); i++)
+        {
+            asistencia_cmp = listaAsistencia.get(i);
+            System.out.println(asistencia_cmp.getResumen());
+        }
+    }
+    public int contarAsistenciaNormales()
+    {
+        int cont = 0;
+        Asistencia asistencia_cmp;
+        for(int i = 0; i < listaAsistencia.size(); i++)
+        {
+            asistencia_cmp = listaAsistencia.get(i);
+            if(asistencia_cmp instanceof AsistenciaNormal)
+            {
+                cont ++;
+            }
+
+        }
+        return cont;
+    }
+    public int contarAsistenciaExtraordinarias()
+    {
+        int cont = 0;
+        Asistencia asistencia_cmp;
+        for(int i = 0; i < listaAsistencia.size(); i++)
+        {
+            asistencia_cmp = listaAsistencia.get(i);
+            if(asistencia_cmp instanceof InasistenciaExtraordinaria)
+            {
+                cont ++;
+            }
+
+        }
+        return cont;
+    }
+    public int contarAsistenciaAnticpadas()
+    {
+        int cont = 0;
+        Asistencia asistencia_cmp;
+        for(int i = 0; i < listaAsistencia.size(); i++)
+        {
+            asistencia_cmp = listaAsistencia.get(i);
+            if(asistencia_cmp instanceof SalidaAnticipada)
+            {
+                cont ++;
+            }
+
+        }
+        return cont;
+    }
+    public int getCantAsistencia()
+    {
+        return  listaAsistencia.size();
+    }
+
+
     
     
 }
