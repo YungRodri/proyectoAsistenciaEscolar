9ea2595720d00129001b22b9d7f20712a43df86a
mateocuetoc-hub <mateo.cueto.c@mail.pucv.cl>
2026-04-11 13:47:35 -0400

    Update Main.java

--- ProyectoProgra/Main.java
+++ ProyectoProgra/Main.java
@@ -1,29 +1,23 @@
-public class Main
 {
     public static void main(String[] args)
     {
-
         Curso curso = new Curso("3A");
 
-        Estudiante e1 = new Estudiante("19.111-1", "Ana", "Rojas", "Perez", 18, "3A");
-        Estudiante e2 = new Estudiante("20.222-2", "Bruno", "Soto", "Diaz", 17, "3A");
-
+        Estudiante e1 = new Estudiante("19.111.111-1", "Ana", "Rojas", "Perez", 18, "3A");
         curso.inscribirEstudiante(e1);
-        curso.inscribirEstudiante(e2);
 
-        Asistencia a1 = new AsistenciaNormal("A1", "10-04-2026", "Sin observación");
-        Asistencia a2 = new InasistenciaExtraordinaria("A2", "11-04-2026", "Resfriado", "Médico");
+        curso.registrarAsistencia("19.111.111-1",
+                new AsistenciaNormal("A1", "10-04-2026", "Sin observación"));
 
-        e1.agregarAsistencia(a1);
-        e1.agregarAsistencia(a2);
+        curso.registrarAsistencia("19.111.111-1",
+                new InasistenciaExtraordinaria("A2", "11-04-2026", "Faltó a clases", "Médico"));
 
-        System.out.println("Historial de " + e1.getNombreCompleto());
+        curso.registrarAsistencia("19.111.111-1",
+                new SalidaAnticipada("A3", "12-04-2026", "Retiro anticipado", "11:30"));
 
-        for (Asistencia a : e1.getListaAsistencia())
-        {
-            System.out.println(a.getResumen());
-        }
-        System.out.println("\nEstudiantes en el curso:");
-        curso.mostrarEstudiantes();
+        curso.mostrarAsistenciasDeEstudiante("19.111.111-1");
+
+        System.out.println("Inasistencias extraordinarias: " + e1.contarAsistenciaExtraordinarias());
+        System.out.println("Salidas anticipadas: " + e1.contarAsistenciaAnticpadas());
     }
 }
