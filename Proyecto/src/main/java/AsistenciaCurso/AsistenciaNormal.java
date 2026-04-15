bfe0c2ac228dfd2294f7ee0b7d002aa4cc71255e
mateocuetoc-hub <mateo.cueto.c@mail.pucv.cl>
2026-04-15 02:13:54 -0400

    Agrego el atributo en AsistenciNormal

--- Proyecto/src/main/java/AsistenciaCurso/AsistenciaNormal.java
+++ Proyecto/src/main/java/AsistenciaCurso/AsistenciaNormal.java
@@ -2,16 +2,29 @@ package AsistenciaCurso;
 
 public class AsistenciaNormal extends Asistencia
 {
+    private boolean puntual;
 
 
-    public AsistenciaNormal(String id, String fecha, String observacion)
+    public AsistenciaNormal(String id, String fecha, String observacion, boolean  puntual)
     {
         super(id, fecha, observacion);
+        this.puntual= puntual;
+    }
+    public boolean isPuntual() {
+        return puntual;
     }
 
+    public void setPuntual(boolean puntual) {
+        this.puntual = puntual;
+    }
+
+
     @Override
-    public String getResumen()
-    {
-        return "Vino a clases el día " + getFecha();
+    public String getResumen() {
+        if (puntual) {
+            return "Vino a clases el día " + getFecha() + " y llegó temprano.";
+        } else {
+            return "Vino a clases el día " + getFecha() + " pero no llegó temprano.";
+        }
     }
 }
